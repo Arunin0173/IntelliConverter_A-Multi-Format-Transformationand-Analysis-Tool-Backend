5aa238aba39a7c1ba1602e8b6f2e015fcc3119af
Arunin0173 <arunrajraj0011@gmail.com>
2024-12-03 22:20:40 +0530

    adding all files

---
+++ src/main/java/com/coverter/intelliconverter/IntelliconverterApplication.java
@@ -0,0 +1,13 @@
+package com.coverter.intelliconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliconverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coverter/intelliconverter/IntelliconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coverter.intelliconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
