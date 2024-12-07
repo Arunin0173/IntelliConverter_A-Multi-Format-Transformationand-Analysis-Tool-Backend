3d96013541bde68ed03d8894deae13e75ab8d8fc
afsal <afsal246@gmail.com>
2024-12-07 15:14:51 +0530

    final change

--- src/main/java/com/coverter/intelliconverter/controller/TextToController.java
+++ src/main/java/com/coverter/intelliconverter/controller/TextToController.java
@@ -17,6 +17,7 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TextToController {	
 	
 	//text to image 
